06f86e4a838623f501ede84737d58940facc6bc1
jahnavid10 <96782459+jahnavid10@users.noreply.github.com>
2023-12-20 15:03:32 +0100

    Delete splitting_data.py

--- splitting_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-# Loading pruned dataset 
-data = pd.read_csv(r'D:\Recommender Systems\Arbeit\project\top_pruned_implicit_feedback.csv')
-
-# Split the data into training and testing sets
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
